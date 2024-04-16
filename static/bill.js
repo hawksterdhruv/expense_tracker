4a65ca8e49787a35b84d653b27501774cbe3ff09
dhruv <hawksterdhruv@gmail.com>
2024-04-15 23:57:04 -0400

    bill form

--- static/bill.js
+++ static/bill.js
@@ -1,42 +1,79 @@
 $(document).ready(function () {
-  var rawData = $("#raw-data");
+  let rawData = $("#raw-data");
+  let table = rawData.children('table');
+  let billImage = rawData.children('img')
   $("#process").click(function () {
     $.ajax({
       type: "GET",
       url: `/api/v1/process/${$(this).data("target")}`,
       contentType: "application/json",
       success: function (data) {
-        rawData.empty();
+        table.empty();
+        billImage.addClass('d-none')
+        billImage.removeClass('d-none').addClass('d-block')
         $.each(data["data"], function (i, a) {
           if (a !== "") {
-            rawData.append(`<tr><td>${a}</td><tr>`);
+            table.append(`<tr><td>${a}</td><tr>`);
           }
         });
       },
     });
   });
-  var thisAmazingFunction = ()=>{
-    console.log(e.target)
+
+  let form = $("#bill-form");
+  let steps = $(".step");
+  let prevBtn = $("#prev-btn");
+  let nextBtn = $("#next-btn");
+  let submitBtn = $("#submit-btn");
+  form.onsubmit = () => {
+    return false;
+  };
+  let currentStep = 0;
+  let stepCount = steps.length;
+  steps.removeClass("d-block").addClass("d-none");
+  $(steps[currentStep]).addClass("d-block");
+  if (currentStep == 0) {
+    prevBtn.addClass("d-none");
+    submitBtn.addClass("d-none");
+    nextBtn.addClass("d-inline-block");
   }
 
-  var fields = ['date', 'total', 'tax', 'items'];
-  var bill = {}
-  var inputField = $('#randomId')
-  // Check global has #process been clicked
-  // logic for early exit ? detect Escape? detect right click ? 
-  // 
-  $("#add-bill").click(function () {
-    fields.forEach(element => {
-        if (element in bill){
-          
-          inputField.attr('placeholder',`Enter ${element}`);
-          inputField.show();
-          rawData.addEventListener('click', thisAmazingFunction, true);
-            // show input box.
-            // allow clicking ? 
-
-        }
-
-    });
+  nextBtn.click(() => {
+    let previousStep = currentStep;
+    currentStep++;
+    if (currentStep > 0 && currentStep < stepCount - 1) {
+      showMiddleStep(currentStep, previousStep);
+    } else if (currentStep == stepCount - 1) {
+      showLastStep(currentStep, previousStep);
+    }
   });
+
+  prevBtn.click(() => {
+    let previousStep = currentStep;
+    currentStep--;
+    if (currentStep > 0 && currentStep < stepCount - 1) {
+      showMiddleStep(currentStep, previousStep);
+    } else if (currentStep == 0) {
+      showMiddleStep(currentStep, previousStep);
+      prevBtn.removeClass("d-inline-block").addClass("d-none");
+    }
+  });
+  function showMiddleStep(currentStep, previousStep) {
+    console.log('showing middle step for step: ', currentStep, ' with prevStep: ', previousStep)
+    prevBtn.removeClass("d-none").addClass("d-inline-block");
+    nextBtn.removeClass("d-none").addClass("d-inline-block");
+    submitBtn.removeClass("d-inline-block").addClass("d-none");
+
+    $(steps[currentStep]).addClass("d-block").removeClass("d-none");
+    $(steps[previousStep]).addClass("d-none").removeClass("d-block");
+  }
+  function showLastStep(currentStep, previousStep) {
+    console.log('showing last step for step: ', currentStep, ' with prevStep: ', previousStep)
+
+    $(steps[currentStep]).removeClass("d-none").addClass("d-block");
+    $(steps[previousStep]).addClass("d-none").removeClass("d-block");
+    prevBtn.removeClass("d-none").addClass("d-inline-block");
+    nextBtn.removeClass("d-inline-block").addClass("d-none");
+    submitBtn.removeClass("d-none").addClass("d-inline-block");
+  }
 });
